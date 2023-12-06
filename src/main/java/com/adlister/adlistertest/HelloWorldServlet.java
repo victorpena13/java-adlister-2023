542de3c5a883e299799d27ce52f5a7d3247bf97c
Victor Pena <13victor.pena@gmail.com>
2023-12-06 13:23:15 -0600

    hello-world can now accept a name parameter if passed

--- src/main/java/com/adlister/adlistertest/HelloWorldServlet.java
+++ src/main/java/com/adlister/adlistertest/HelloWorldServlet.java
@@ -8,14 +8,20 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello-world")
+@WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello")
 public class HelloWorldServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html");
         PrintWriter out = response.getWriter();
-        out.println("<h1>Hello, victor!</h1>");
+
+        String name = request.getParameter("name");
+        if(name==null) {
+            out.println("<h1>Hello, World</h1>");
+        } else {
+            out.println("<h1>Hello, " + name + "</h1>");
+        }
     }
 
 
