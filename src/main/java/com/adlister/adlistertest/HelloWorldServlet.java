066b895194b06f5926a4166aa5c7cfc6c2918b34
Victor Pena <13victor.pena@gmail.com>
2023-12-06 13:59:08 -0600

    completed bonus and added a counter to show many times a page has been viewed

--- src/main/java/com/adlister/adlistertest/HelloWorldServlet.java
+++ src/main/java/com/adlister/adlistertest/HelloWorldServlet.java
@@ -5,24 +5,27 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import static java.lang.System.out;
+
 @WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello")
 public class HelloWorldServlet extends HttpServlet {
-
+    public int counter;
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html");
         PrintWriter out = response.getWriter();
-
         String name = request.getParameter("name");
         if(name==null) {
             out.println("<h1>Hello, World</h1>");
+            counter++;
         } else {
             out.println("<h1>Hello, " + name + "</h1>");
+            counter++;
         }
+        out.println(counter);
     }
-
-
 }
