8dcd6f0594389a87ca3129ad6e5ff44712a7f311
Victor Pena <13victor.pena@gmail.com>
2024-01-12 15:20:43 -0600

    created a bean that represents album. includes a zero-argument constructor, has getters & setters. implements Serializable

---
+++ src/main/java/com/adlister/adlistertest/model/Album.java
@@ -0,0 +1,63 @@
+package com.adlister.adlistertest.model;
+
+import java.io.Serializable;
+
+public class Album implements Serializable {
+
+    private long id;
+    private String artist;
+    private String record_name;
+    private String release_date;
+    private float sales;
+    private String genre;
+
+    public Album() {}
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public String getRecord_name() {
+        return record_name;
+    }
+
+    public void setRecord_name(String record_name) {
+        this.record_name = record_name;
+    }
+
+    public String getRelease_date() {
+        return release_date;
+    }
+
+    public void setRelease_date(String release_date) {
+        this.release_date = release_date;
+    }
+
+    public float getSales() {
+        return sales;
+    }
+
+    public void setSales(float sales) {
+        this.sales = sales;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+}
